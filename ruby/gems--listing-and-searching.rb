a4680c45e21c25912d3dce5a4c389586ff670104
Joe Miragliuolo <sod@llawn.com>
2023-11-16 11:27:22 -0500

    Updated rubygems listing and searching

--- ruby/gems--listing-and-searching.rb
+++ ruby/gems--listing-and-searching.rb
@@ -14,10 +14,7 @@ puts 'SEARCHING (pop):'
 
 search_term = 'Pop'
 
-specs = Gem::Specification.find_all do |spec|
-  spec.name == "net-#{search_term.downcase}"
-  # spec.name == "minitest" # This is an example with multiple versions on my system.
-end
+specs = Gem::Specification.find_all_by_name("net-#{search_term.downcase}")
 
 # In case there are multiple versions, sort the returned array & take last item
 spec = specs.sort_by { |spec| spec.version }.last
